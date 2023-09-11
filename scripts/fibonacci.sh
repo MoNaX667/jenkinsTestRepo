04ad64b04bdf71575c79b2f4d7fb6e966d6fc7d0
Vitalii Beliakov <vitalii_beliakov@epam.com>
2023-09-12 00:37:40 +0600

    Uniting arguments for echo command

--- scripts/fibonacci.sh
+++ scripts/fibonacci.sh
@@ -1,22 +1,22 @@
 # Program for Fibonacci
 # Series
-  
+
 # Static input for N
 maxValue=$1
- 
+
 # First Number of the
 # Fibonacci Series
 firstNumber=0
- 
+
 # Second Number of the
 # Fibonacci Series
 secondNumber=1
-  
+
 echo "Generating Fibonacci series for : "
-  
+
 for (( i=0; i<maxValue; i++ ))
 do
-    echo -n -e "$((i + 1))) $firstNumber \n"
+    echo -ne "$((i + 1))) $firstNumber \n"
     sum=$((firstNumber + secondNumber))
     firstNumber=$secondNumber
     secondNumber=$sum
