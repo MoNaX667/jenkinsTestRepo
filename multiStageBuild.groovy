7936b6c06b964274dfa342760444116b61b1f1bd
Vitalii Beliakov <vitalii_beliakov@epam.com>
2023-09-11 23:31:42 +0600

    Adding an extra pipeline with multi stages

---
+++ multiStageBuild.groovy
@@ -0,0 +1,23 @@
+pipeline{
+    agent any
+
+    stages{
+        stage('TEST'){
+            steps{
+                echo "Testing data and changes"
+            }
+        }
+        stage('DEPLOY'){
+            steps{
+                input message: "Do you want to deploy changes for production env?", ok: 'Deploy to Production'
+                echo "Deploying the changes since it is Production trigger"
+            }
+        }
+        stage('REPORT'){
+            steps{
+                sh "echo 'Getting changelog data into artifact' > my-result.txt"
+                archiveArtifacts allowEmptyArchive: true, artifacts: '*.txt', fingerprint: true, followSymlinks: false, onlyIfSuccessful: true
+            }
+        }
+    }
+}
